66419ebaec9c3ee1c523102a3b76cad774df2ab6
cyvac1 <cyvac1@jinhakapply.com>
2023-11-17 14:10:21 +0900

    feat: 라우팅 기능 추가

--- template/src/App.js
+++ template/src/App.js
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<>App</>} />
+        <Route path="/test" element={<>Test</>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
