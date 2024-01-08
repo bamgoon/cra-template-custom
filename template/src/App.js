75ba9ceabd2f58687a6b5bd990a1b735e84599a2
cyvac1 <cyvac1@jinhakapply.com>
2024-01-08 14:56:48 +0900

    feat: MainPage 추가

--- template/src/App.js
+++ template/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainPage from 'pages/MainPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<>App</>} />
-        <Route path="/test" element={<>Test</>} />
+        <Route path="*" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
